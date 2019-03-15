e390dc9a5f11655307e377da8d4135c2ee24dd06
Rogério Alencar Lino Filho <rogeriolino@gmail.com>
2019-03-15 19:13:35 -0300

    sf4.1 deprecations fix

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -26,7 +26,7 @@ use Novosga\ReportsBundle\Form\ReportType;
 use Novosga\Service\AtendimentoService;
 use Novosga\Service\UsuarioService;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Translation\TranslatorInterface;
 
@@ -34,7 +34,7 @@ use Symfony\Component\Translation\TranslatorInterface;
  *
  * @author Rogerio Lino <rogeriolino@gmail.com>
  */
-class DefaultController extends Controller
+class DefaultController extends AbstractController
 {
     const DOMAIN = 'NovosgaReportsBundle';
     const MAX_RESULTS = 1000;
